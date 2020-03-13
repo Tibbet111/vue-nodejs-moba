f560c376b2bb26a40ff98643dd3d90816a905e0a
“Tibbet111 <928775138@qq.com>
2020-03-13 22:13:32 +0800

    1

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>首页</div>
+  <div>
+    <swiper  ref="mySwiper" :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide><img class="w-100" src="../assets/image/1.jpg"></swiper-slide>
+    <swiper-slide><img class="w-100" src="../assets/image/2.jpg"></swiper-slide>
+    <swiper-slide><img class="w-100" src="../assets/image/3.jpg"></swiper-slide>
+    <div class="swiper-pagination pagination-home text-right px-3 pb-2"  slot="pagination"></div>
+  </swiper>
+  </div>
 </template>
 
 <script>
@@ -7,13 +15,34 @@ export default {
   name: '',
   data() {
     return {
+      swiperOption:{
+        pagination: {
+            el: '.pagination-home',
+          },
+          loop:true,
+          speed:500,
+          autoplay : {
+            delay:3000
+          },
+          
+      }
     };
   },
   methods: {},
 };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import '../assets/scss/_variables.scss';
+.pagination-home{
+  .swiper-pagination-bullet{
+    opacity: 1;
+    background: #fff;
+    border-radius: 0.1538rem;
+      &.swiper-pagination-bullet-active{
+        background: map-get($map: $colors, $key: 'info');
+      }
+  } 
+}
 </style>
 
